20614a3dcc2ab6bdb08a38f3ba2093dd98e88d97
Alexander Hedges <ahedges@ethz.ch>
2016-10-20 21:47:08 +0200

    implement todos
    Still needs some testing though

--- expander.rkt
+++ expander.rkt
@@ -28,10 +28,12 @@
 
 ;; Parse Tree Functions
 
-(define (rex test1 [separator ":"] [test3 (void)])
-  test1
-  ;; TODO: Resolve references
-  test3)
+(define (rex implicit-part [separator ":"] [explicit-part (void)])
+  implicit-part
+  (resolve-refs (map (lambda (node)
+                       (car node))
+                     (gvector->list node-vector)))
+  explicit-part)
 (provide rex)
 
 ;; we pass around one index and two stacks
@@ -52,21 +54,24 @@
 
 (define-macro (transition CHAR)
   #'(lambda (index [first-nodes void] [fallbacks void])
-      (if (list? fallbacks)
-          (begin ;; in implicit expression
-            (let ([current-node (gvector-ref node-vector index)])
+      (let ([current-node (gvector-ref node-vector index)])
+        (if (list? fallbacks)
+            (begin ;; in implicit expression
               (gvector-set! node-vector index `(,(car current-node)
-                                                ,(cons `(,CHAR ,(add1 index)) (cadr current-node))
+                                                ,(cons `(,CHAR ,(add1 index)) (cadr current-node)) ;; BUG: modify due to *
                                                 ,(caddr current-node)
-                                                ,(cadddr current-node))))
-            (gvector-add! node-vector `(,(number->string (add1 index))
-                                        ()
-                                        ,(car fallbacks);; BUG: find first number that is not nil 
-                                        #f))
-            `(,(add1 index) ,first-nodes ,fallbacks))
-          (begin ;; else (explicit expression)
-            ;; TODO: create new transition
-            `(,index)))))
+                                                ,(cadddr current-node)))
+              (gvector-add! node-vector `(,(number->string (add1 index))
+                                          ()
+                                          ,(car fallbacks);; BUG: find first number that is not nil 
+                                          #f))
+              `(,(add1 index) ,first-nodes ,fallbacks))
+            (begin ;; else (explicit expression)
+              (gvector-set! node-vector index `(,(car current-node)
+                                                ,(cons `(,CHAR) (cadr current-node))
+                                                ,(caddr current-node)
+                                                ,(cadddr current-node)))
+              `(,index))))))
 (provide transition)
 
 (define (character char)
@@ -78,17 +83,27 @@
       (void (fold-funcs '(0) (filter procedure? (list NODE-LINE ...))))))
 (provide explicit-expression)
 
-(define-macro (node-line CONTENT ...)
+(define-macro (node-line TRANSITIONS ...)
     #'(lambda (index)
-        ;; TODO: call node "ident"
-        (void (fold-funcs `(,index) (cdr (filter procedure? (list CONTENT ...)))))
+        (void (fold-funcs `(,index) (cdr (filter procedure? (list TRANSITIONS ...)))))
         `(,(add1 index))))
 (provide node-line)
 
 (define-macro (node-identifier IDENT-LIST)
   #'(lambda (index)
-      ;; put in target of last transition
-      (void (display IDENT-LIST))
+      (let ([current-node (gvector-ref node-vector index)])
+        (let ([current-transition (caadr current-node)])
+          (if (equal? 1 (length current-transition))
+              (begin
+                (gvector-set! node-vector index `(,(car current-node)
+                                                  ,(cons `(,(car current-transition) ,(string-append IDENT-LIST)) (cdadr current-node))
+                                                  ,(caddr current-node)
+                                                  ,(cadddr current-node))))
+              (begin ;; otherwise we are looking at node naming
+                (gvector-set! node-vector index `(,(string-append IDENT-LIST)
+                                                  ,(cadr current-node)
+                                                  ,(caddr current-node)
+                                                  ,(cadddr current-node)))))))
       `(,index)))
 (provide node-identifier)
 
@@ -110,6 +125,7 @@
 ;; Expression Matching
 
 (define (match-input string-list state-index)
+  (display state-index)
   (if (empty? string-list) (if (cadddr (gvector-ref node-vector state-index)) #t
                                #f)
       (let ([new-state (calculate-new-state (car string-list) state-index)])
@@ -125,3 +141,26 @@
           (if (equal? 0 (length taken-transition)) (caddr current-node)
               (begin (display "Error: Non-deterministic expression")
               -1))))))
+
+(define (resolve-refs names)
+  (for/list ([i (in-range (sub1 (gvector-count node-vector)))])
+    (let ([element (gvector-ref node-vector i)])
+      (gvector-set! node-vector i `(,(car element)
+                                    ,(resolved-transitions (cadr element) names)
+                                    ,(caddr element)
+                                    ,(cadddr element))))))
+
+(define (resolved-transitions transitions names)
+  (if (empty? transitions) '()
+      (cons (resolve-transition (car transitions) names) (resolved-transitions (cdr transitions) names))))
+
+(define (resolve-transition transition names)
+  (if (not (string? cadr)) `(,(car transition) ,(index-of (cadr transition) names))
+      transition))
+
+(define (index-of elem list)
+  (if (empty? list) -1
+      (if (equal? elem (car list)) 0
+          (let ([index (index-of elem (cdr list))])
+            (if (equal? -1 index) -1
+                (add1 index))))))
